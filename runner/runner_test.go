3b0db9fde52bcde4c2b1cc1eb98e98a55e0cc853
Mario Nitchev <marionitchev@gmail.com>
2019-11-05 15:13:11 +0200

    Refactor tests
    [#169446487]

--- runner/runner_test.go
+++ runner/runner_test.go
@@ -12,22 +12,22 @@ import (
 var _ = Describe("Runner", func() {
 	When("running a command", func() {
 		It("should run it in a new UTS namespace", func() {
-			buf := bytes.Buffer{}
+			output := bytes.Buffer{}
 			container := runner.Container{
 				Command: "readlink",
 				Args:    []string{"-n", "/proc/self/ns/uts"},
 				Stdin:   os.Stdin,
-				Stdout:  &buf,
+				Stdout:  &output,
 				Stderr:  os.Stderr,
 			}
 
 			parentUts, err := os.Readlink("/proc/self/ns/uts")
 			Expect(err).NotTo(HaveOccurred())
+			containerUts := output.String()
 
 			err = runner.Run(container)
 			Expect(err).NotTo(HaveOccurred())
-			Expect(buf.String()).NotTo(Equal(parentUts))
+			Expect(containerUts).NotTo(Equal(parentUts))
 		})
-
 	})
 })
