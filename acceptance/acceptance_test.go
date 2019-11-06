50e0438a693c63f25a483c43d6303799b5090eb1
Mario Nitchev <marionitchev@gmail.com>
2019-11-06 23:01:20 +0200

    Add acceptance test for pid namespace [#169446489]

--- acceptance/acceptance_test.go
+++ acceptance/acceptance_test.go
@@ -2,6 +2,7 @@ package acceptance_test
 
 import (
 	"os/exec"
+	"strings"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -14,7 +15,8 @@ var _ = Describe("Acceptance", func() {
 		cmd := exec.Command(barrelPath, "roll", "--rootfs", rootfsPath, "/bin/sh", "--", "-c", "hostname foo; hostname")
 		session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 		Expect(err).ToNot(HaveOccurred())
-		Eventually(session.Out).Should(gbytes.Say("foo"))
+		Eventually(session.ExitCode).Should(Equal(0))
+		Expect(session.Out).To(gbytes.Say("foo"))
 	})
 
 	It("should exit with the exit code of the container", func() {
@@ -29,5 +31,18 @@ var _ = Describe("Acceptance", func() {
 		session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 		Expect(err).ToNot(HaveOccurred())
 		Eventually(session.ExitCode).Should(Equal(1))
+	})
+
+	It("should not be able to list the host's processes", func() {
+		cmd := exec.Command(barrelPath, "roll", "-r", rootfsPath, "/bin/sh", "--", "-c", "ps", "aux")
+		session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+		Expect(err).ToNot(HaveOccurred())
+
+		Eventually(session.ExitCode).Should(Equal(0))
+		output := strings.Trim(string(session.Out.Contents()), "\n")
+		lines := strings.Split(output, "\n")
+		Expect(lines).To(HaveLen(3))
+		Expect(lines).To(ContainElement(MatchRegexp("1.*/bin/sh -c ps")))
+		Expect(lines).To(ContainElement(MatchRegexp("[0-9]+.*ps")))
 	})
 })
